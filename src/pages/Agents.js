39644b88dd50d14ce5b24ad1cac280457429bada
JESKA JENIPHER <jeska.jenipher@student.moringaschool.com>
2022-09-12 12:26:24 +0300

    added various functions to different components

--- src/pages/Agents.js
+++ src/pages/Agents.js
@@ -1,88 +1,57 @@
-import { useEffect, useState, React } from "react";
-import { Table, TableCell, TableRow, TableHead, TableBody, Button, ThemeProvider } from "@mui/material";
-// import { createTheme, styled, ThemeProvider } from "@mui/material/styles";
-
+import React, { useEffect, useState } from "react";
+import TeacherForm from "../Components/TeacherForm";
+import TeachersDetails from "../Components/TeachersDetails";
+//We need all teachers to be displayed here
+//We need to edit teachers details, add, or delete
 
 const Agents = () => {
-  const [agents, setAgents] = useState([]);
+  const [allAgents, setAllAgents] = useState([]);
 
-  const url = "https://sporty-sport.herokuapp.com/agents";
-
-
-  //CRUD-GET
-  const getData = () => {
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setAgents(data);
-      });
-  };
   useEffect(() => {
-    getData();
+    fetch("https://sporty-sport.herokuapp.com/agents")
+      .then((r) => r.json())
+      .then((data) => setAllAgents(data));
   }, []);
 
-  //DELETE
+  // console.log(allTeachers)
 
-  function deleteAgent(id) {
+  function handlePosting(data) {
+    setAllTeachers([...allAgents, data]);
+  }
+
+  function deleteTeacher(id) {
     fetch(`https://sporty-sport.herokuapp.com/agents/${id}`, {
       method: "DELETE",
     })
       .then((r) => r.json())
       .then(() => {
-        const goThru = agents.filter((agent) => agent.id !== id);
-        setAgents(goThru);
+        const goThru = allAgents.filter((agent) => agent.id !== id);
+        setAllAgents(goThru);
       });
   }
 
-  //POST
+  function handleUpdateAgent(updatedAgent) {
+    const updatedAgent = allAgents.map((agent) => {
+      if (agent.id === updatedAgent.id) {
+        return updatedAgent;
+      } else {
+        return agent;
+      }
+    });
+    setAllAgents(updatedAgent);
+  }
 
-  //Styling..
-  const tableStyle = {
-    width: "80%",
-    margin: "20px 10%",
-  };
-
-  //  const darkTheme = createTheme({
-  //    palette: {
-  //      mode: "light",
-  //    },
-  //  });
   return (
-    <>
-      {/* <ThemeProvider theme={darkTheme}> */}
-        <Table style={tableStyle}>
-          <TableHead>
-            <TableRow style={{ fontSize: "18px" }}>
-              <TableCell>ID</TableCell>
-              <TableCell>Name</TableCell>
-              <TableCell>ID_Number</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {agents.map((agent) => (
-              <TableRow key={agent.id}>
-                <TableCell>{agent.id}</TableCell>
-                <TableCell>{agent.name}</TableCell>
-                <TableCell>{agent.id_number}</TableCell>
-                <TableCell>
-                  <Button
-                    variant="contained"
-                    color="secondary"
-                    style={{ margin: "0px 20px" }}
-                    onClick={() => {
-                      deleteAgent(agent.id);
-                    }}
-                  >
-                    Delete
-                  </Button>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      {/* </ThemeProvider> */}
-    </>
+    <div>
+      <AgentForm
+        handlePosting={handlePosting}
+        onUpdateAgent={handleUpdateAgent}
+      />
+      <AgentsDetails
+        allAgents={allAgents}
+        deleteAgent={deleteAgent}
+      />
+    </div>
   );
 };
 
